61577503728632dcc5e37be0517953628c63e5f0
mnagovicz <ema2026.ai@gmail.com>
2026-05-17 20:15:30 +0200

    fix: correct API URL

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'https://v1lprtd415.execute-api.eu-west-1.amazonaws.com/prod';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://e4che8h7sd.execute-api.eu-west-1.amazonaws.com/prod';
 export const ADMIN_KEY = 'aospoint2026';
